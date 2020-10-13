48dafbb151191cbf2b3f3d484da13b207ec58a20
leena0203 <leenasarode02@gmail.com>
2020-10-14 02:14:56 +0530

    Aded: Peak to return the top element and pop to return last node

--- src/main/java/com/MyNode/MyStack.java
+++ src/main/java/com/MyNode/MyStack.java
@@ -2,19 +2,33 @@ package com.MyNode;
 
 public class MyStack {
 	public final MyLinkedList myLinkedList;
-	
+
 	/**
 	 * Constructor
 	 */
 	public MyStack() {
 		this.myLinkedList = new MyLinkedList();
 	}
-	
+	/**
+	 * Push to stack(UC1)
+	 * @param myNode
+	 * @return
+	 */
 	public boolean push(INode myNode) {
 		myLinkedList.add(myNode);
 		return true;
 	}
-
+	
+	/**
+	 * Peek UC2
+	 * @return
+	 */
+	public INode peak() {
+		return myLinkedList.head;
+	}
+	public INode pop() {
+		return myLinkedList.pop();
+	}
 	public void printMyStack() {
 		myLinkedList.printMyNode();
 	}
--- src/test/java/com/MyNode/MyStackTest.java
+++ src/test/java/com/MyNode/MyStackTest.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyStackTest {
-
+	/**
+	 * UC1 check test to add elements in stack from top
+	 */
 	@Test
 	void given3NumbersWhenAddedToStackShouldBeAddedToTop() {
 		MyNode<Integer> firstElement = new MyNode<>(70);
@@ -17,7 +19,43 @@ class MyStackTest {
 		assertTrue(test.push(secondElement));
 		assertTrue(test.push(thirdElement));
 		test.printMyStack();
-		
 	}
+
+	/**
+	 * UC2 Return top element when peek
+	 */
+	@Test
+	void given3NumbersWhenPeekShoulReturnTopNode() {
+		MyNode<Integer> firstElement = new MyNode<>(70);
+		MyNode<Integer> secondElement = new MyNode<>(30);
+		MyNode<Integer> thirdElement = new MyNode<>(56);
+		MyStack test = new MyStack();
+		test.push(firstElement);
+		test.push(secondElement);
+		test.push(thirdElement);
+		test.printMyStack();
+		INode<Integer> peak = test.peak();
+		assertEquals(thirdElement,peak);
+
+	}
+	
+	/**
+	 * Pop Stack
+	 */
+	@Test
+	void given3NumbersWhenPopShoulReturnLastNode() {
+		MyNode<Integer> firstElement = new MyNode<>(70);
+		MyNode<Integer> secondElement = new MyNode<>(30);
+		MyNode<Integer> thirdElement = new MyNode<>(56);
+		MyStack test = new MyStack();
+		test.push(firstElement);
+		test.push(secondElement);
+		test.push(thirdElement);
+		test.printMyStack();
+		INode<Integer> pop = test.pop();
+		assertEquals(thirdElement,pop);
+		test.printMyStack();
+	}
+
 
 }
